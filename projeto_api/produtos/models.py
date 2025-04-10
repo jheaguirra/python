ddebd9383339e3e3ab19151389780489d6adfab1
jheaguirra <jhenifermartins.dev2024@gmail.com>
2025-04-10 14:08:22 -0300

    Django - API projeto

---
+++ projeto_api/produtos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+        
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    categoria = models.ForeignKey(Categoria, related_name='produtos', on_delete=models.CASCADE)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+ 
+    def __str__(self):
+        return self.nome
