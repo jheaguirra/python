851037d2649ece3624849a322caf84077509f126
jheaguirra <jhenifermartins.dev2024@gmail.com>
2025-04-15 15:48:56 -0300

    api

---
+++ frontend/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    fetchProdutos();
+});
+
+function fetchProdutos (){
+    fetch("http://localhost:8000/api/produtos/")
+    .then(res => res.json())
+    .then(data => renderProdutos(data))
+    .catch(err => console.error("erro ao buscar produtos", err));
+}
+function renderProdutos(produtos){
+
+    const container = document.getElementById("produtos-container");
+    container.innerHTML = "";
+
+    produtos.forEach(produto =>{
+        const card = document.createElement('div');
+        card.className = "produto";
+        card.innerHTML = `
+            <h2>${produto.nome}</h2>
+            <p>${produto.descricao}</P>
+            <p class="preco"> R$ ${produto.preco}</p>
+            <p><strong>Categoria:</strong> ${produto.categoria.nome}</p>
+        `;
+    container.appendChild(card);
+});
+}
