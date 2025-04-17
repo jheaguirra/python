d521ec9b95b56119c682ae59ccb7538f9493fc07
jheaguirra <jhenifermartins.dev2024@gmail.com>
2025-04-17 10:09:07 -0300

    css

--- projeto_api/produtos/models.py
+++ projeto_api/produtos/models.py
@@ -12,6 +12,7 @@ class Produto(models.Model):
     descricao = models.TextField()
     categoria = models.ForeignKey(Categoria, related_name='produtos', on_delete=models.CASCADE)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
  
     def __str__(self):
         return self.nome
