f1f6edc06976022382db50149391b96c92e1ce23
jheaguirra <jhenifermartins.dev2024@gmail.com>
2025-04-17 10:40:19 -0300

    produtos

--- frontend/script.js
+++ frontend/script.js
@@ -6,25 +6,27 @@ function fetchProdutos (){
     fetch("http://localhost:8000/api/produtos/")
     .then(res => res.json())
     .then(data => renderProdutos(data))
-    .catch(err => console.error("erro ao buscar produtos", err));
+    .catch(err=> console.error("Erro ao buscar produtos", err));
+
 }
+
 function renderProdutos(produtos){
 
     const container = document.getElementById("produtos-container");
     container.innerHTML = "";
 
-    produtos.forEach(produto =>{
-        const card = document.createElement('div');
+    produtos.forEach(produto => {
+        const card = document.createElement("div");
         card.className = "produto";
         card.innerHTML = `
-            <div class="imagem-container">
-                <img src="${produto.imagem}" alt="${produto.nome}" /> 
-            </div>
             <h2>${produto.nome}</h2>
-            <p>${produto.descricao}</P>
+                        <div class="imagem-container">
+                <img src="${produto.imagem}" alt="${produto.nome}" />
+            </div>
+            <p>${produto.descricao}</p>
             <p class="preco"> R$ ${produto.preco}</p>
             <p><strong>Categoria:</strong> ${produto.categoria.nome}</p>
         `;
-    container.appendChild(card);
-});
+        container.appendChild(card);
+    });
 }
